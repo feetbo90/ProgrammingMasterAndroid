584c7ebd6c7a0f430a2ea6f5f7a5fb71e16f22ec
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-03-12 18:45:13 +0700

    Baru Relative Dan Linear

---
+++ app/src/main/java/contoh/master/programming/programmingmaster01/linearDanRelative.kt
@@ -0,0 +1,12 @@
+package contoh.master.programming.programmingmaster01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class linearDanRelative : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_dan_relative)
+    }
+}
