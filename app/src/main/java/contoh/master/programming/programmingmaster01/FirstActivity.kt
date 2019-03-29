505fb1c433e99171c0f651340d6f5fb5a0e1bb70
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-03-29 19:38:32 +0700

    Belajar Intent

--- app/src/main/java/contoh/master/programming/programmingmaster01/FirstActivity.kt
+++ app/src/main/java/contoh/master/programming/programmingmaster01/FirstActivity.kt
@@ -9,6 +9,7 @@ class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_first)
 
 
