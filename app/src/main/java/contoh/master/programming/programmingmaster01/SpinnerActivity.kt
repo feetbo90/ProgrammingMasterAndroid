8b15fa9c5e24df248546263716290a821f46cd7d
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-04-16 19:05:34 +0700

    spinner

---
+++ app/src/main/java/contoh/master/programming/programmingmaster01/SpinnerActivity.kt
@@ -0,0 +1,51 @@
+package contoh.master.programming.programmingmaster01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_spinner.*
+
+class SpinnerActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spinner)
+
+        val adapter = ArrayAdapter.createFromResource(this, R.array.planets_array,
+                android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        planets_spinner.adapter = adapter
+        planets_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //var planets = resources.getStringArray(R.array.planets_array)
+                Log.d("masuk", " ini masuk ")
+                setImage("${parent!!.getItemAtPosition(position)}")
+                Toast.makeText(applicationContext, "ini pilihan anda ${parent.getItemAtPosition(position)}", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+/*
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        //var planets = resources.getStringArray(R.array.planets_array)
+        Log.d("masuk", " ini masuk ")
+        Toast.makeText(applicationContext, "ini pilihan anda ${parent!!.getItemAtPosition(position)}", Toast.LENGTH_LONG).show()
+    }*/
+
+    fun setImage(gambar : String) {
+        if(gambar.trim() == "Earth")
+        image.setImageResource(R.drawable.bumi)
+        else
+            image.setImageResource(R.mipmap.ic_launcher)
+    }
+}
